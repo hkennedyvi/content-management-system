868f9a5baa7982eba88b4985560b5ae6ae23ea67
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-29 20:05:11 -0800

    Bugs fixed

--- inquire.js
+++ inquire.js
@@ -2,7 +2,6 @@ const inquirer = require("inquirer");
 const Employee = require("./employee");
 const mysql = require("mysql");
 const addedEmployees = [];
-
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,17 +10,33 @@ const connection = mysql.createConnection({
     database: "companyDB"
 });
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("Error Connecting: " + err.stack);
-        return;
-    }
+startApp = () => {
 
-    console.log("Connected as ID: " + connection.threadId);
-});
+    connection.connect(function (err) {
+        if (err) {
+            console.error("Error Connecting: " + err.stack);
+            return;
+        }
+
+        console.log("Connected as ID: " + connection.threadId);
+    })
+    setTimeout(() => {
+        inquirer.prompt([
+            {
+                type: "confirm",
+                name: "start",
+                message: "Welcome, are you ready to begin?"
+            }
+        ]).then(function (answer) {
+            if (answer.start === true) {
+                return promptUser();
+            } else console.log("Goodbye");
+        })
+    }, 100)
+}
 
 // id first_name last_name title department salary manager
-function promptUser() {
+promptUser = () => {
     return inquirer.prompt([
         {
             type: "list",
@@ -29,6 +44,8 @@ function promptUser() {
             message: "What would you like to do?",
             choices: [
                 "View all employees",
+                "View all departments",
+                "View all roles",
                 "View all employees by department",
                 "View all employees by manager",
                 "Add employee",
@@ -39,42 +56,39 @@ function promptUser() {
                 "Add Role"
             ]
         }
-    ]).then(function (response) {
+    ]).then((response) => {
         switch (response.task) {
             case "View all employees":
-                console.log("Viewing employees");
                 showEmployees();
                 break;
+            case "View all departments":
+                showDepartments();
+                break;
+            case "View all roles":
+                showRoles();
+                break;
             case "View all employees by department":
-                console.log("Viewing department employees");
                 showEmployeesByDept();
                 break;
             case "View all employees by manager":
-                console.log("Viewing manager employees");
                 showEmployeesByMgr();
                 break;
             case "Add employee":
-                console.log("Add employee");
                 addEmployee();
                 break;
-            case "Remove Employee":
-                console.log("Remove Employee");
+            case "Remove employee":
                 removeEmployee();
                 break;
             case "Update Employee Role":
-                console.log("Update Role");
                 updateEmployee();
                 break;
             case "Update Employee Manager":
-                console.log("Update Manager");
                 updateMgr();
                 break;
             case "Add Department":
-                console.log("Adding department");
                 addDepartment();
                 break;
             case "Add Role":
-                console.log("Adding role");
                 addRole();
                 break;
             default:
@@ -82,14 +96,41 @@ function promptUser() {
 
         };
     });
-
+    //WORKS!
     function showEmployees() {
         const query = "SELECT * FROM companyDB.employee_table";
         connection.query(query, (err, res) => {
             if (err) throw err;
+            console.log("\n EMPLOYEES AT THIS AGENCY:");
             for (var i = 0; i < res.length; i++) {
-                console.log("First: " + res[i].first_name + "\n Last: " + res[i].last_name + "\n\n");
-              }
+                console.log("\n First: " + res[i].first_name + "\n Last: " + res[i].last_name + "\n");
+            }
+            promptUser();
+        });
+
+    };
+    //WORKS!
+    function showDepartments() {
+        const query = "SELECT * FROM companyDB.department_table";
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.log("\n DEPARTMENTS AT THIS AGENCY:");
+            for (var i = 0; i < res.length; i++) {
+                console.log("\n Department: " + res[i].name + "\n");
+            }
+            promptUser();
+        });
+
+    };
+    //WORKS!
+    function showRoles() {
+        const query = "SELECT * FROM companyDB.role_table";
+        connection.query(query, (err, res) => {
+            console.log("\n ROLES AT THIS AGENCY:");
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++) {
+                console.log("\n Title: " + res[i].title + "\n");
+            }
             promptUser();
         });
 
@@ -97,12 +138,14 @@ function promptUser() {
 
     function showEmployeesByDept() {
         console.log("Here are your employees by department");
+        promptUser();
     };
 
     function showEmployeesByMgr() {
         console.log("Here are your employees by manager");
+        promptUser();
     };
-
+    //WORKS!
     function addEmployee() {
         inquirer.prompt([
             {
@@ -116,11 +159,11 @@ function promptUser() {
             }, {
                 type: "input",
                 name: "title",
-                message: "What is their title?"
+                message: "What is their role ID#?"
             }, {
                 type: "input",
                 name: "department",
-                message: "What is their department?"
+                message: "What is their department ID#?"
             }, {
                 type: "input",
                 name: "salary",
@@ -128,9 +171,9 @@ function promptUser() {
             }, {
                 type: "input",
                 name: "manager",
-                message: "Who is their manager?"
+                message: "What is their manager's ID# (type null if no manager)?"
             }
-        ]).then(function (answers) {
+        ]).then((answers) => {
 
             const newEmployee = new Employee(
                 answers.first_name,
@@ -143,17 +186,17 @@ function promptUser() {
 
             addedEmployees.push(newEmployee);
             console.log(addedEmployees);
-            console.log(addedEmployees[0].firstName);
-            const query = "INSERT INTO employee_table (first_name, last_name) VALUES (?, ?)";
-            connection.query(query, [addedEmployees[0].firstName, addedEmployees[0].lastName], (err, res) => {
+
+            const query = "INSERT INTO employee_table (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+            connection.query(query, [addedEmployees[0].firstName, addedEmployees[0].lastName, addedEmployees[0].title, addedEmployees[0].department], (err, res) => {
                 if (err) throw err;
-                console.log("Employee added to database");
+                console.log("\n EMPLOYEE ADDED \n");
                 promptUser();
             });
 
         })
     };
-
+    //WORKS!
     function removeEmployee() {
         inquirer.prompt([
             {
@@ -165,31 +208,42 @@ function promptUser() {
                 name: "last_name",
                 message: "What is their last name?"
             }
-        ]).then(function (answer) {
+        ]).then((answer) => {
 
             const query = "DELETE FROM employee_table WHERE (first_name=? AND last_name=?)"
             connection.query(query, [answer.first_name, answer.last_name], (err, res) => {
                 if (err) throw err;
-                console.log("DELETED");
+                console.log("\n DELETED \n");
                 promptUser();
             })
         })
     };
-
+    //WORKS!
     function updateEmployee() {
         inquirer.prompt([
             {
                 type: "input",
-                name: "employee_name",
-                message: "Which employee would you like to update?"
+                name: "first_name",
+                message: "What is the first name of the employee you would like to update?"
             }, {
                 type: "input",
-                name: "new_department",
-                message: "What is their new department?"
+                name: "last_name",
+                message: "What is their last name?"
+            }, {
+                type: "input",
+                name: "new_role",
+                message: "What is their new role ID#?"
             }
-        ])
+        ]).then((answers) => {
+            const query = "UPDATE employee_table SET role_id=? WHERE (first_name=? AND last_name=?)"
+            connection.query(query, [answers.new_role, answers.first_name, answers.last_name], (err, res) => {
+                if (err) throw err;
+                console.log("\n UPDATED ROLE \n");
+                promptUser();
+            });
+        })
     };
-
+    //WORKS!
     function updateMgr() {
         inquirer.prompt([
             {
@@ -203,11 +257,18 @@ function promptUser() {
             }, {
                 type: "input",
                 name: "new_manager",
-                message: "Who is their new manager?"
+                message: "What is their new manager's ID# (type null if no manager)?"
             }
-        ])
+        ]).then((answers) => {
+            const query = "UPDATE employee_table SET manager_id=? WHERE (first_name=? AND last_name=?)"
+            connection.query(query, [answers.new_manager, answers.first_name, answers.last_name], (err, res) => {
+                if (err) throw err;
+                console.log("\n UPDATED MANAGER \n");
+                promptUser();
+            });
+        })
     };
-
+    //WORKS!
     function addDepartment() {
         inquirer.prompt([
             {
@@ -215,36 +276,40 @@ function promptUser() {
                 name: "new_department",
                 message: "What department would you like to add?"
             }
-        ]).then(function (answer) {
+        ]).then((answer) => {
             const query = "INSERT INTO department_table (name) VALUES (?)";
             connection.query(query, [answer.new_department], (err, res) => {
                 if (err) throw err;
-                console.log("Department added to database");
+                console.log("\n DEPARTMENT ADDED \n");
                 promptUser();
             })
         })
     };
-
+    //WORKS!
     function addRole() {
         inquirer.prompt([
             {
                 type: "input",
                 name: "new_role",
-                message: "What role would you like to add?"
+                message: "What job title would you like to add?"
             }, {
                 type: "input",
                 name: "new_salary",
-                message: "What is the salary?"
+                message: "What is the salary for this position?"
+            }, {
+                type: "input",
+                name: "new_department",
+                message: "What is the department ID#?"
             }
-        ]).then(function (answers) {
+        ]).then((answers) => {
             const query = "INSERT INTO role_table (title, salary, department_id) VALUES (?, ?, ?)";
-            connection.query(query, [answers.new_role, answers.new_salary, 50], (err, res) => {
+            connection.query(query, [answers.new_role, answers.new_salary, answers.new_department], (err, res) => {
                 if (err) throw err;
-                console.log("Role added to database");
+                console.log("\n ROLE ADDED \n");
                 promptUser();
             })
         })
     };
 }
 
-promptUser();
+startApp();
